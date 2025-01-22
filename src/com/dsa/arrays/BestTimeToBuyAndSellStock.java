55329c793f58922dcb04fe781c89ef2615aa0986
mjags <jagadeeshpavan.m@gmail.com>
2025-01-22 07:54:42 -0500

    added BestTimeToBuyAndSellStock

---
+++ src/com/dsa/arrays/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+package com.dsa.arrays;
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        int min = prices[0];
+        int max = Integer.MIN_VALUE;
+
+        for (int price : prices) {
+            int money = price - min;
+            if (money > max) max = money;
+            if (price < min) min = price;
+        }
+
+        return max;
+    }
+    public static void main(String[] args){
+        BestTimeToBuyAndSellStock stock = new BestTimeToBuyAndSellStock();
+        System.out.println(stock.maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+}
